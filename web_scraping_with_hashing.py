e09cc08f33f3cba4acb974dcb3d57c4c3b6a7cfe
LuboD911 <79481701+LuboD911@users.noreply.github.com>
2021-07-24 13:21:04 +0300

    Update web_scraping_with_hashing.py

--- web_scraping_with_hashing.py
+++ web_scraping_with_hashing.py
@@ -1,107 +1,123 @@
 from bs4 import BeautifulSoup
 import requests
-import re
 import docx
 from hash_function import hashing
+import os
+import time
+
+#You must enable the access for less secure apps in your google account.
+#Otherwise you will get an error.
+def send_notification_email(gmail:str,password:str):
+    import smtplib
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+
+    server.login(gmail, password)
+
+    message = 'Just updated the docx file.'
+    # your email, the other person's email( in that case you want to send the email to yourself)
+    server.sendmail(gmail, gmail, message )
+
+    server.quit()
 
 
-url = 'https://aws.amazon.com/new/?whats-new-content-all'
+while True:
 
-response = requests.get(url)
-# print(response)
+    url = 'https://aws.amazon.com/new/?whats-new-content-all'
 
-
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-titles = soup.find_all("h5", {"class": "lb-txt-bold lb-txt-uppercase lb-txt-white lb-none-v-margin lb-h5 lb-title"})
-info = soup.find_all("div", {"class": "lb-txt-normal lb-txt-white lb-rtxt"})
-dates = soup.find_all("div", {"class": "lb-tiny-align-right lb-txt-bold lb-txt-none lb-txt-white lb-txt"})
-
-titles_string_list = [str(title) for title in titles]
-info_string_list = [str(inf) for inf in info]
-dates_string_list = [str(date) for date in dates]
-
-regex_title = ">\s.*<"
-
-filtered_titles_list = []
-
-for text in titles_string_list:
-    match = re.findall(regex_title, text)
-    match = match[0]
-    # match = match[2:-1]
-    match = match.strip('>')
-    match = match.strip('<')
-    match = match.strip(' ')
-    filtered_titles_list.append(match)
+    response = requests.get(url)
+    # print(response)
 
 
+    is_added = False
 
-regex_info = ">.*\."
 
-filtered_info_list = []
+    soup = BeautifulSoup(response.text, "html.parser")
 
-for text in info_string_list:
-    match = re.findall(regex_info, text)
-    match = match[0]
-    # match = match[1:-1]
-    match = match.strip('>')
-    match = match.strip('<')
-    filtered_info_list.append(match)
+    titles = soup.find_all("h5", {"class": "lb-txt-bold lb-txt-uppercase lb-txt-white lb-none-v-margin lb-h5 lb-title"})
+    info = soup.find_all("div", {"class": "lb-txt-normal lb-txt-white lb-rtxt"})
+    dates = soup.find_all("div", {"class": "lb-tiny-align-right lb-txt-bold lb-txt-none lb-txt-white lb-txt"})
+
+    titles_string_list = [title.text.strip() for title in titles]
+    info_string_list = [inf.text.strip() for inf in info]
+    dates_string_list = [date.text.strip() for date in dates]
+
+
+    all_in_one = [(titles_string_list[i],info_string_list[i],dates_string_list[i]) for i in range(len(dates_string_list))]
+    hashed_objects = []
+
+
+    for el in all_in_one:
+        title = el[0]
+        text = el[1]
+        date = el[2]
+
+        hash_value = hashing(title,text,date)
+        hashed_objects.append(hash_value)
 
 
 
-regex_date = "\s[A-Z].*\\n"
 
-filtered_dates_list = []
+    if not os.path.isfile("D:/my_written_file.docx"):
+        mydoc = docx.Document()
 
+        for i in range(len(titles_string_list)):
+            print(f"Title: {titles_string_list[i]}")
+            mydoc.add_heading(titles_string_list[i])
+            print(f"Text: {info_string_list[i]}")
+            mydoc.add_paragraph(info_string_list[i])
+            print(f"Date: {dates_string_list[i]}")
+            mydoc.add_paragraph(dates_string_list[i])
+            print(f"Hash value: {hashed_objects[i]}")
+            print()
+            mydoc.add_paragraph(f"")
 
-for text in dates_string_list:
-    match = re.findall(regex_date, text)
-    match = match[0]
-    match = match.strip('\n')
-    match = match.strip(' ')
-    # match = match[1:-2]
-    filtered_dates_list.append(match)
+        mydoc.save("D:/my_written_file.docx")
 
-
-all_in_one = [(filtered_titles_list[i],filtered_info_list[i],filtered_dates_list[i]) for i in range(len(filtered_dates_list))]
-hashed_objects = []
-
-
-for el in all_in_one:
-    title = el[0]
-    text = el[1]
-    date = el[2]
-
-    hash_value = hashing(title,text,date)
-    hashed_objects.append(hash_value)
-
-
-filtered_hashed_objects = []
-
-for i in range(len(hashed_objects)):
-    if hashed_objects[i] not in filtered_hashed_objects:
-        filtered_hashed_objects.append(hashed_objects[i])
     else:
-        filtered_titles_list.pop(i)
-        filtered_info_list.pop(i)
-        filtered_dates_list.pop(i)
+        mydoc = docx.Document("D:/my_written_file.docx")
 
-mydoc = docx.Document()
+        article = []
 
-for i in range(len(filtered_dates_list)):
-    print(f"Title: {filtered_titles_list[i]}")
-    mydoc.add_heading(filtered_titles_list[i])
-    print(f"Text: {filtered_info_list[i]}")
-    mydoc.add_paragraph(filtered_info_list[i])
-    print(f"Date: {filtered_dates_list[i]}")
-    mydoc.add_paragraph(filtered_dates_list[i])
-    print(f"Hash value: {filtered_hashed_objects[i]}")
-    print()
-    mydoc.add_paragraph(f"")
+        articles = []
 
-try:
-    mydoc.save("D:/my_written_file.docx")
-except:
-    print('!!!Затвори word файла за да може да бъде ъпдейтнат'.upper())
+        for i, el in enumerate(mydoc.paragraphs):
+            el = el.text
+
+            article.append(el)
+            if (i + 1) % 4 == 0:
+                articles.append(article)
+                article = []
+
+        for article in articles:
+            title, text, date, nothing = article
+
+            hash_value = hashing(title, text, date)
+            if hash_value not in hashed_objects:
+                hashed_objects.append(hash_value)
+
+                print(f"Title: {title}")
+                mydoc.add_heading(title)
+                print(f"Text: {text}")
+                mydoc.add_paragraph(text)
+                print(f"Date: {date}")
+                mydoc.add_paragraph(date)
+                print(f"Hash value: {hash_value}")
+                print()
+                mydoc.add_paragraph(f"")
+                is_added = True
+
+
+        if is_added:
+            gmail = 'Write your gmail here'
+            password = 'Write your password here'
+            send_notification_email(gmail, password)
+
+        try:
+            mydoc.save("D:/my_written_file.docx")
+        except:
+            print('!!!Затвори word файла за да може да бъде ъпдейтнат'.upper())
+
+
+    seconds = 6000
+    time.sleep(seconds)
